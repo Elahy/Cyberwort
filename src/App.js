5a96e9580018d03b5101950477c88ecf45aa1e2f
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2021-08-10 11:24:37 +0600

    background done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+// import Button from '@material-ui/core/Button';
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: "100vh",
+    backgroundImage: `url(${
+      process.env.PUBLIC_URL + "/images/workstation.jpg"
+    })`,
+  },
+  menuButton: {
+    marginRight: "5%",
+    color: "white",
+  },
+  title: {
+    flexGrow: 1,
+    color: "white",
+    fontFamily: "'Varela Round', sans-serif",
+    fontSize: 20,
+    marginLeft: "5%",
+  },
+  header: {
+    color: "#282c34",
+    backgroundColor: "#282c34",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        CyberWort
+    <div className={classes.root}>
+      <header className="">
+        <div>
+          <AppBar className={classes.header} position="static">
+            <Toolbar>
+              <Typography variant="h6" className={classes.title}>
+                Cyberwort
+              </Typography>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </div>
       </header>
+      <main>
+        <CssBaseline />
+        <h1 className="tag">Cyberwort : Software from Future</h1>
+      </main>
+      <footer></footer>
     </div>
   );
 }
